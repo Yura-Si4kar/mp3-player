ee8fbb64e567a6eea1872ce2443bc2bc23f3217b
Sichkar Yurii <si4kar2017@gmail.com>
2024-01-28 15:50:03 +0200

    bgc

--- src/components/albums/AlbumsList.js
+++ src/components/albums/AlbumsList.js
@@ -4,7 +4,7 @@ import AlbumsItem from '../items/AlbumsItem'
 export default function AlbumsList({ albums }) {
     return (
         <div className='d-flex justify-content-center flex-wrap'>
-            {albums.map((album) => (<AlbumsItem key={album.id} album={album}/>))}
+            {albums.length > 0 ? albums.map((album) => (<AlbumsItem key={album.id} album={album}/>)) : 'Додайте альбом'}
         </div>
     )
 }
